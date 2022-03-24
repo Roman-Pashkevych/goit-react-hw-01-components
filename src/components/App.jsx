a778145b35ee07520ed1070eb4aa47190e01a068
Roman-Pashkevych <pashkevych_roman@ukr.net>
2022-03-25 00:44:44 +0200

    Соціальний профіль розмітка

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,27 @@
+// import FriendList from './FriendList/FriendList';
+import SocialProfile from './SocialProfile/SocialProfile';
+// import Statistics from './Statistics/Statistics';
+// import TransactionHistory from './TransactionHistory/TransactionHistory';
+// import friends from './FriendList/friends.json';
+import user from './SocialProfile/user.json';
+// import data from './Statistics/data.json';
+// import transactions from './TransactionHistory/transactions.json';
+
 export const App = () => {
+  return (
+    <>
+      <SocialProfile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+};
+
+/* export const App = () => {
   return (
     <div
       style={{
@@ -15,3 +38,4 @@ export const App = () => {
     </div>
   );
 };
+ */
